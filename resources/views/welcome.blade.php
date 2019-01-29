6a35b3f605bfbde1aab69bb5a544eef07fe1408d
Russi4nBe4r <mterek97@gmail.com>
2019-01-29 22:53:26 +0300

    Models path, User event token, API Token model + migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    dev.simplegame.buhojmedved
                 </div>
 
                 <div class="links">
